803f7c55e476c1978fa7b9b32024cb57a5dfd7a9
Giordano Dìaz <diazgiordano@gmail.com>
2020-09-25 10:51:43 -0500

    Add validiation to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :name, :email, :password, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: true
+  validates :password, length: { minimum 6 }
   has_many :post
   has_many :comment
 end
