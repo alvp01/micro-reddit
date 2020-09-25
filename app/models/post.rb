46177a9531884ab999e10da020909db4df31a088
Giordano Dìaz <diazgiordano@gmail.com>
2020-09-25 11:48:06 -0500

    Delete unnecesary empty spaces on files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
-
 class Post < ApplicationRecord
   validates :title, :body, presence: true
   validates :title, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, :email, :password, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: true
-  validates :password, length: { minimum 6 }
+  validates :password, length: { minimum: 6 }
   has_many :post
   has_many :comment
 end
